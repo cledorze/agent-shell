0a7c0580c0033e97a88aab797c600ff377ad36e2
tofix <tofix@uterrie.cledorze.lan>
2025-03-18 01:13:41 +0100

    update cleaning

--- updated_routes.py
+++
@@ -1,7 +0,0 @@
-# Import existing routes code and replace VM manager import with:
-from robust_vm_manager import RobustVMManager
-
-# Then initialize the VM manager:
-vm_manager = RobustVMManager()
-
-# The rest of your routes code remains the same
